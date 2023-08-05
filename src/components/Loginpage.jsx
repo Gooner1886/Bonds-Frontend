8a76a5848209fb3f42f6437dc73d4c32c0981950
Srishti <srishtiagg23@gmail.com>
2023-08-06 00:39:20 +0530

    Initialized App with login page

---
+++ src/components/Loginpage.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import './loginpage.css'
+
+const LoginPage = () => {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleLogin = (event) => {
+    event.preventDefault();
+    
+    console.log('Login data:', formData);
+  };
+
+  return (
+    <div className="App">
+      <div className="login-container">
+        <h2>Welcome!</h2>
+        <form onSubmit={handleLogin}>
+          <div className="form-group">
+            <label htmlFor="email">Email:</label>
+            <input
+              type="email"
+              name="email"
+              value={formData.email}
+              onChange={handleInputChange}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password:</label>
+            <input
+              type="password"
+              name="password"
+              value={formData.password}
+              onChange={handleInputChange}
+              required
+            />
+          </div>
+          <button className='loginbutton' type="submit">Login</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
