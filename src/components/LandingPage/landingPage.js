0c07324203cb0eb4569f671218f6d8c873c76c1a
Siddharth Sarma <siddharthgsarma5@gmail.com>
2023-08-09 00:48:52 +0530

    dynamically fetching top bonds

--- src/components/LandingPage/landingPage.js
+++ src/components/LandingPage/landingPage.js
@@ -1,10 +1,27 @@
 import React from "react";
+import { useEffect, useState } from "react";
 import Navbar from "../Navbar/Navbar";
 import BondCarousel from "./../BondCarousel/bondCarousel";
 import TopBonds from "../TopBonds/TopBonds";
 import BondInfo from "../BondInfo/BondInfo";
+import axios from "axios";
 
-const landingPage = () => {
+const LandingPage = () => {
+  const [bondData, setBondData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:9010/bonds/security")
+      .then((response) => {
+        // console.log(response);
+        setBondData(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+
   return (
     <>
       <Navbar style={{ margin: 0 }} />
@@ -13,11 +30,11 @@ const landingPage = () => {
         <img src="https://via.placeholder.com/1600x300" alt="placeholder" />
         <img src="https://via.placeholder.com/1600x300" alt="placeholder" />
       </BondCarousel>
-      <TopBonds />
+      <TopBonds bondinfo={bondData} />
       <BondInfo />
       {/* <Footer /> */}
     </>
   );
 };
 
-export default landingPage;
+export default LandingPage;
--- src/components/TopBonds/TopBonds.js
+++ src/components/TopBonds/TopBonds.js
@@ -1,41 +1,43 @@
 import React from "react";
 import Card from "../Card/Card";
 import "./TopBonds.css";
+import { useState } from "react";
 
-const TopBonds = () => {
+let dataToBeSorted = [];
+let limitedData = [];
+
+const TopBonds = (props) => {
+  const sortingHandler = () => {
+    console.log(props.bondinfo);
+    let sortData = [];
+    props.bondinfo.sort((a, b) => b.coupon - a.coupon);
+    for (let obj of props.bondinfo) {
+      sortData.push(obj);
+    }
+    const limit = 5;
+    limitedData = sortData.slice(0, limit);
+  };
+
+  sortingHandler();
+  console.log(limitedData);
   return (
-    <div className="top-bonds">
-      <Card
-        image="https://blog.investingnote.com/wp-content/uploads/2018/09/Bonds.jpg"
-        title="Bond 1"
-        description="Bond 1 details"
-      ></Card>
-      <Card
-        image="https://blog.investingnote.com/wp-content/uploads/2018/09/Bonds.jpg"
-        title="Bond 2"
-        description="Bond 2 details"
-      ></Card>
-      <Card
-        image="https://blog.investingnote.com/wp-content/uploads/2018/09/Bonds.jpg"
-        title="Bond 3"
-        description="Bond 3 details"
-      ></Card>
-      <Card
-        image="https://blog.investingnote.com/wp-content/uploads/2018/09/Bonds.jpg"
-        title="Bond 4"
-        description="Bond 4 details"
-      ></Card>
-      <Card
-        image="https://blog.investingnote.com/wp-content/uploads/2018/09/Bonds.jpg"
-        title="Bond 5"
-        description="Bond 5 details"
-      ></Card>
-      {/* <Card
-        image="https://via.placeholder.com/1600x300"
-        title="Bond 6"
-        description="Bond 6 details"
-      ></Card> */}
-    </div>
+    <>
+      <div className="heading">
+        <h3>Hand Picked Bonds</h3>
+      </div>
+      <div className="top-bonds">
+        {limitedData.map((bond) => {
+          console.log(bond.issuer);
+          return (
+            <Card
+              image="https://blog.investingnote.com/wp-content/uploads/2018/09/Bonds.jpg"
+              title={bond.issuer}
+              description={bond}
+            ></Card>
+          );
+        })}
+      </div>
+    </>
   );
 };
 
